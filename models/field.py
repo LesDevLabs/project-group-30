0a5a50ec34373b2ab71959fd2a08f2c7b1db193e
mepov-gl <mykola.epov@globallogic.com>
2025-11-13 11:17:58 +0200

    g-30_10 add validation to phone and email. Improve Field class with setter and getter

--- models/field.py
+++ models/field.py
@@ -4,3 +4,11 @@ class Field:
 
     def __str__(self):
         return str(self.value)
+    
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
