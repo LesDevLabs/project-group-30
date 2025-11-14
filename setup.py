be56d4038010d2858c6ec14bcf0a905766975cab
froii <froii@ukr.net>
2025-11-14 15:16:41 +0200

    fix pathes to main

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import find_packages, setup
 
 setup(
     name="assistant-bot-G30",
-    version="0.1.1",
+    version="0.1.5",
     author="Group 30 (2025)",
     author_email="lestyshchenko@gmail.com, sagepol@gmail.com, 444ten@gmail.com, koliaepov@gmail.com, valerian.demichev@gmail.com, 3t.zarudenska@gmail.com",
     description="Асистент-бот для управління контактами з підтримкою різних форматів збереження",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/LesDevLabs/project-group-30",
-    packages=find_packages(),
+    packages=["cli", "handlers", "models", "repositories", "search", "storage", "utils"],
+    py_modules=["main"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -18,7 +19,7 @@ setup(
     install_requires=["colorama", "rich"],
     entry_points={
         "console_scripts": [
-            "assistant-bot-G30=assistant_bot_g30.cli_entry:run",
+            "assistant-bot-G30=main:main",
         ],
     },
 )
